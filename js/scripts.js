47e1d06f6c115fdee7469e92556b2a05538a3ff9
epcruz <eduardaffpaulo@gmail.com>
2022-02-09 11:21:48 +0100

    set Pokemon list

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,17 @@
-alert('Hello world');
-
-let favoriteFood = 'Curry Wurst';
-document.write(favoriteFood);
+let pokemonList = [
+    {
+        name: "Pikachu",
+        height: 1.04,
+        types: ['Electric']
+    },
+    {
+        name: "Ivysaur",
+        height: 3.03,
+        types: ['grass','poison']
+    },
+    {
+        name: "Beedrill",
+        height: 3.03,
+        types: ['bug','poison']
+    }
+]
