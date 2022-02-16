6970457c3f517be216e85d4b821177e10eaf6964
epcruz <eduardaffpaulo@gmail.com>
2022-02-16 22:23:13 +0100

    IIFE wrapping

--- js/scripts.js
+++ js/scripts.js
@@ -1,27 +1,42 @@
-let pokemonList = [
-    {
-        name: "Pikachu",
-        height: 1.04,
-        types: ['Electric']
-    },
-    {
-        name: "Pidgey",
-        height: 1.00,
-        types: ['normal','flying']
-    },
-    {
-        name: "Beedrill",
-        height: 3.03,
-        types: ['bug','poison']
-    }
-]
-    //writing the name and height of the pokemons
-    pokemonList.forEach(function(pokemon) {
-        document.write('<p class = "pokemon-list">'+ pokemon.name + ' (height: ' + pokemon.height + ') ');
-    //condicional highlighting the biggest one      
-        if (pokemon.height >=3){
-         document.write( ' ' + '<span>Wow, that’s big!</span>');
+let pokemonRepository = (function () {
+    let pokemonList = [
+        {
+            name: "Pikachu",
+            height: 1.04,
+            types: ['Electric']
+        },
+        {
+            name: "Pidgey",
+            height: 1.00,
+            types: ['normal', 'flying']
+        },
+        {
+            name: "Beedrill",
+            height: 3.03,
+            types: ['bug', 'poison']
         }
-      });
+    ]
+
+        function add(pokemon) {
+            pokemonList.push(pokemon);
+        }
     
-    document.write(pokemonList.forEach());
+        function getAll() {
+            return pokemonList;
+        }
+    
+        return {
+            add: add,
+            getAll: getAll
+        };
+    })();
+
+     //writing the name and height of the pokemons
+    pokemonRepository.getAll().forEach(function(pokemon){
+        document.write('<p class = "pokemon-list">' + pokemon.name + ' (height: ' + pokemon.height + ')'); {
+            //condicional highlighting the biggest one     
+            if (pokemon.height >= 3)
+                document.write(' ' + '<span>Wow, that’s big!</span>');
+            }
+      });
+     
